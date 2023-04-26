e3b686d7e910a66c96c09e0992fb896a4365e629
Allas Assis <allasassis10@gmail.com>
2023-04-26 15:04:04 -0300

    Project created

---
+++ src/main/java/com/delivery/food_api/FoodApiApplication.java
@@ -0,0 +1,13 @@
+package com.delivery.food_api;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FoodApiApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(FoodApiApplication.class, args);
+    }
+
+}
